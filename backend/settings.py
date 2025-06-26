7b92950dede7381a8b44d7c64a2fff3342f4a455
mhdatheek136 <87757769+mhdatheek136@users.noreply.github.com>
2025-06-26 12:07:08 +0500

    minor changes

--- backend/settings.py
+++ backend/settings.py
@@ -25,13 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g86o!2v#cr_da(k(&&1-utwx)vyxhp$smxh(+6%k6o59=g5$3b'
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-g86o!2v#cr_da(k(&&1-utwx)vyxhp$smxh(+6%k6o59=g5$3b'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-key")
 DEBUG = os.getenv("DEBUG", "False") == "True"
